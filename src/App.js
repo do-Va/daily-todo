307fd7c0f2d835eef9598c47157a1d0cf10b473c
doVa (Doğukan Variş) <varisdogukan@outlook.com>
2022-11-26 10:22:57 +0300

    Started Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components/macro";
+import TodoContainer from "./components/TodoContainer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper className="flex-center">
+      <TodoContainer />
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  height: 100%;
+  background-color: var(--bg);
+`;
 
 export default App;
